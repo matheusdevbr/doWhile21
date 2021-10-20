1eaa47b550ab0b976ee813ee9b4919cab88b7da9
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-10-20 13:44:01 -0300

    Add e configurando o websocket

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,28 @@
 //mural de depoimentos
 import 'dotenv/config'; 
 import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
+
+
 import { router } from './routes';
 
 const app = express();
+app.use(cors())
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on('connection', socket => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+})
+
 app.use(express.json());
 
 app.use(router);
